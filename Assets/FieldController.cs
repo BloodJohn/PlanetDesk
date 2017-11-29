4deae92c0c71f4aac40e50a0e7ff53eab7345642
Eugene Pavlov <bjohn.ext@gmail.com>
2017-11-29 20:11:02 +0300

    add project

---
+++ Assets/FieldController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    public const int maxCost = 10000;
+    public const int topCount = 20;
+
+    [SerializeField] private PlanetController planetPrefab;
+
+    private readonly List<PlanetController> viewList = new List<PlanetController>(topCount);
+    private readonly List<PlanetController> delList = new List<PlanetController>(topCount);
+    private readonly List<PlanetData> cellList = new List<PlanetData>();
+
+    private int fieldWidth = 7;
+    private Vector2Int fieldPos = Vector2Int.zero;
+
+
+    private void Start()
+    {
+        fieldPos = new Vector2Int(0, 0);
+
+        UpdatePlanetField();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            fieldPos.y++;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            fieldPos.y--;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            fieldPos.x++;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            fieldPos.x--;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            fieldWidth = Mathf.Min(maxCost, fieldWidth + 2);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            fieldWidth = Mathf.Max(7, fieldWidth - 2);
+        }
+
+        else
+        {
+            return;
+        }
+
+
+
+
+        UpdatePlanetField();
+    }
+
+    private void UpdatePlanetField()
+    {
+        //сносим все планеты с поля
+        while (viewList.Count > 0)
+        {
+            var delItem = viewList[0];
+            viewList.Remove(delItem);
+            delItem.gameObject.SetActive(false);
+            delList.Add(delItem);
+
+        }
+
+
+        for (int x = -fieldWidth / 2; x <= fieldWidth / 2; x++)
+            for (int y = -fieldWidth / 2; y < fieldWidth / 2; y++)
+            {
+                var newPos = new Vector2Int(x + fieldPos.x, y + fieldPos.y);
+                var newData = cellList.FirstOrDefault(t => t.pos.x == newPos.x && t.pos.y == newPos.y);
+                //если это новая клетка
+                if (newData == null)
+                {
+                    newData = new PlanetData
+                    {
+                        cost = -1,
+                        pos = newPos
+                    };
+                    if (Random.value <= 0.3f) newData.cost = Random.Range(0, maxCost);
+                    cellList.Add(newData);
+                }
+
+                //создаем планету
+                if (newData.cost >= 0)
+                {
+                    PlanetController newPlanet;
+                    if (delList.Count > 0)
+                    {
+                        newPlanet = delList[0];
+                        delList.Remove(newPlanet);
+                    }
+                    else
+                    {
+                        newPlanet = (PlanetController)Instantiate(planetPrefab, transform);
+
+                    }
+                    newPlanet.Init(newData, fieldPos);
+
+                    viewList.Add(newPlanet);
+                }
+            }
+    }
+}
---
+++ Assets/PlanetController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    public PlanetData data;
+
+    public void Init(PlanetData newData, Vector2Int shift)
+    {
+        gameObject.SetActive(true);
+        data = newData;
+
+        transform.localPosition = new Vector3(data.pos.x - shift.x, data.pos.y - shift.y, 0);
+        name = string.Format("planet {0},{1}", data.pos.x, data.pos.y);
+    }
+}
+
+/// <summary>Каждой планете случайно присваивается “рейтинг” от 0 до 10 000, который отображается числом над планетой.</summary>
+public class PlanetData
+{
+    public Vector2Int pos;
+    public int cost;
+}
